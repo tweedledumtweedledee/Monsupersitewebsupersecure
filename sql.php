6ba10e6c3289ea2089f1a915fbd0ef961531eb13
Tweedledum Tweedledee <tdumtdee@wonderland.com>
2018-03-10 12:16:06 -0500

    ajout bootstrap et déplacé flag dans flag.html

--- sql.php
+++ sql.php
@@ -1,6 +1,7 @@
-<!--site html pour SQl injection du club de sécurité-->
+<!--https://github.com/tweedledumtweedledee/Monsupersitewebsupersecure.git-->
 <!DOCTYPE html>
 <?php
+if (isset($_POST['id'])){
 $config = parse_ini_file('config.ini');
 $dbusername = $config['username'];
 $dbpass = $config['password'];
@@ -12,27 +13,54 @@ $query = "SELECT count(*) FROM login WHERE username='{$id}'and password='{$passw
 $db = new PDO("mysql:dbname=$dbname;host=$dbhost", $dbusername, $dbpass);
 $result = $db->query($query);
 if(($result->fetch(PDO::FETCH_NUM))[0]==0) {
-    echo'Mauvais mot passe!';
+    echo '<div class ="alert alert-danger"role="alert">Mauvais mot passe!</div>';
 }
 else{
-echo '<script>alert("FLAG")</script>';
+header("Location:flag.html"); 
+exit; // <- don't forget this!
 }
- 
+ }
 ?>
 <html>
 <meta charset="utf-8"/>
 <head>
-<title>SQL Injection</title>
-</head>
- 
-<body>
-<h1>Bienvenue à l'épreuve de recrutement du club de sécurité de Rosemont</h1>
-<p style="color:green;">Le but de l'épreuve est de trouver le nom de L'utilisateur et le mot de passe.</p>
-<form method=POST>
-   <label for="user">Nom d'utilisateur</label>
-   <input type="text" id="id" name="id">
-   <label for="pass">Password</label>
-   <input type="password" id="pass" name="pass">
-   <button type="submit">Entrer</button>
-</form>
 
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
+<title>L'autre côté du miroir</title>
+</head>
+<style>
+form, body, p { text-align: center;}
+</style> 
+<body >
+ <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<h1>Bienvenue de l'autre côté du miroir!</h1>
+<p style="color:green;">L'autre du miroir vous donne accès a un monde fantastique qui dépasse les frontières de l'imagination!</p>
+
+<form method=POST>
+ <p>
+   <label for="user">Nom d'utilisateur</label>
+ </p>
+ <p>
+   <input type="text" id="id" name="id">
+ </p>
+ <p>
+   <label for="pass">Password</label>
+ </p>
+ <p>
+   <input type="password" id="pass" name="pass">
+ </p>
+ <p>
+   <button type="submit">Entrer</button>
+ </p>
+</form>
+</body>
